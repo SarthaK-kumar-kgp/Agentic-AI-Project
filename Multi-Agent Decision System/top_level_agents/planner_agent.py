8f408a42eb5a24cc39e95d6a5c1597f1dec81bac
Sarthak <79709691+SarthaK-kumar-kgp@users.noreply.github.com>
2026-08-14 15:27:39 +0800

    add planner agent

---
+++ Multi-Agent Decision System/top_level_agents/planner_agent.py
@@ -0,0 +1,31 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from .prompts import *
+import json
+load_dotenv()
+
+api_key = os.getenv("DEEPSEEK_API_KEY")
+
+client = OpenAI(api_key=api_key,base_url="https://api.deepseek.com")
+
+def planning_agent(original_question:str,context_items:dict):
+    prompt = PLANNER_AGENT
+    payload = {
+        "original_question": original_question,
+        "enriched_context": context_items
+    }
+    response = client.chat.completions.create(
+        model  = "deepseek-v4-flash",
+        messages=[
+            {"role": "system", "content":prompt},
+            {"role": "user", "content":json.dumps(payload,indent = 2)}
+
+        ],
+        temperature=0.3,
+        max_tokens = 1000,
+        extra_body={
+                    "thinking": {"type": "disabled"}
+                }
+    )
+    return response.choices[0].message.content.strip()
